c7be0ae774e023b1c2bf7f8106aaf1f41e20d71c
François Saint-Jacques <fsaintjacques@gmail.com>
2012-08-12 00:33:05 -0400

    moved functions

--- shells/common/functions.sh
+++ shells/common/functions.sh
@@ -69,21 +69,3 @@ function xml() {
   cat $tmpfile | pbcopy
   rm $tmpfile
 }
-
-function hostmatch() {
-    if [[ `hostname --fqdn` =~ $1 ]]; then
-        return 0
-    fi
-    return 1
-}
-
-dseq() {
-    start=$1;
-    end=$2;
-    echo $start;
-
-    while [ "$start" != "$end" ] ; do
-        start=`date +"%Y-%m-%d" -d "$start+ 1 day"`;
-        echo "$start";
-    done
-}
--- shells/common/helpers.sh
+++ shells/common/helpers.sh
@@ -1,3 +1,21 @@
 function osx {
   [[ `uname -s` == 'Darwin' ]]
 }
+
+function hostmatch() {
+    if [[ `hostname --fqdn` =~ $1 ]]; then
+        return 0
+    fi
+    return 1
+}
+
+function dseq() {
+    start=$1;
+    end=$2;
+    echo $start;
+
+    while [ "$start" != "$end" ] ; do
+        start=`date +"%Y-%m-%d" -d "$start+ 1 day"`;
+        echo "$start";
+    done
+}
