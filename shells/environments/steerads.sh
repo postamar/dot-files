f9516ac4252a4c7998b03719b88083df82fc9ce0
François Saint-Jacques <fsaintjacques@gmail.com>
2012-08-13 09:59:12 -0400

    Added steerads env

---
+++ shells/environments/steerads.sh
@@ -0,0 +1,5 @@
+hostmatch steerads-desktop1.steerads.com || return
+
+export PATH=/data/src/steerads-collaborative-git/bin:/data/src/steerads-collaborative-git/cron:$PATH
+export PYTHONPATH=/data/src/steerads/src/py-steerads:/data/src/jobman:/data/src/theano
+export STEERADS_HOME=/data/src/steerads
