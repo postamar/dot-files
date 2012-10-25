acbe5f6dbffcddef66fa98910226f6245f5d8e7e
François Saint-Jacques <fsaintjacques@gmail.com>
2012-10-25 13:52:38 -0400

    fixed ssh-agent plugin configuration

--- shells/zsh/oh-my-zsh.zsh
+++ shells/zsh/oh-my-zsh.zsh
@@ -1,6 +1,8 @@
 ZSH="$SHELL_FILES/lib/oh-my-zsh"
 ZSH_THEME="cloud"
 
+zstyle :omz:plugins:ssh-agent identities id_rsa
+
 plugins=(git git-flow python screen ssh-agent virtualenvwrapper)
 
 # Comment this out to disable weekly auto-update checks
