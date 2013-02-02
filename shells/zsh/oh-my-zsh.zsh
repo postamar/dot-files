4befe5991c9fcaa7f104fb7e093cdb67eb590707
Marius Posta <mariusposta@gmail.com>
2013-02-01 19:09:40 -0500

    added modified lambda theme

--- shells/zsh/oh-my-zsh.zsh
+++ shells/zsh/oh-my-zsh.zsh
@@ -1,27 +1,12 @@
 ZSH="$SHELL_FILES/lib/oh-my-zsh"
-ZSH_THEME="clean"
+ZSH_THEME="lambda_marius"
 
 zstyle :omz:plugins:ssh-agent identities id_rsa
 
-plugins=(git git-flow python screen ssh-agent virtualenvwrapper tmux)
+plugins=(git git-flow python screen ssh-agent tmux)
 
 # Comment this out to disable weekly auto-update checks
 DISABLE_AUTO_UPDATE="true"
 
 source $ZSH/oh-my-zsh.sh
 
- # git theming
-PROMPT='%{$fg[$NCOLOR]%}%B%n%b%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} $(git_prompt_info)%(!.#.$) '
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg_no_bold[yellow]%}%B"
-ZSH_THEME_GIT_PROMPT_SUFFIX=" $(git_prompt_status)%b%{$fg_bold[blue]%})%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_CLEAN=""
-ZSH_THEME_GIT_PROMPT_DIRTY=""
-ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg_bold[red]%}⇡"
-ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg_bold[red]%}⇣"
-ZSH_THEME_GIT_PROMPT_DIVERGED="%{$fg_bold[red]%}⭠"
-ZSH_THEME_GIT_PROMPT_ADDED="%{$fg_bold[green]%}✚"
-ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}✹"
-ZSH_THEME_GIT_PROMPT_DELETED="%{$fg_bold[red]%}✖"
-ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg_bold[red]%}➜"
-ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg_bold[red]%}═"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}✭"
